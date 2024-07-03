b47c526e255ac46e35f3c296d1d4b7c090fb0361
novicer88 <vitalikasmar@gmail.com>
2024-07-03 20:20:44 +0300

    Modify server response

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.send('Hello World from the Express server');
 });
 
 app.listen(3000);
